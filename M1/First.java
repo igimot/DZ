153a93aedf4909429c64f3116e9f80d025c720bf
iGi.mot <oleg.chulanov@gmail.com>
2016-06-03 23:39:42 +0300

    M1

--- M1/First.java
+++ M1/First.java
@@ -1,7 +1,7 @@
 /**
  * Created by Oleg on 18.05.2016.
  */
-public class first {
+public class First {
     public static void main(String[] arg){
         System.out.println("Hi ther its my first coding in java. Coool");
         System.out.println("and");
